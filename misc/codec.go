88f6723e9200f814ec6e8239f37d807722233281
Parham Alvani <parham.alvani@gmail.com>
2018-08-06 09:07:45 +0430

    Improves codec sender.

--- misc/codec.go
+++ misc/codec.go
@@ -14,28 +14,44 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"strings"
 )
 
 func main() {
+	projectID := "hello"
 
-	url := "http://185.116.162.237:8080/api/runners/hello/codec"
+	url := fmt.Sprintf("http://185.116.162.237:8080/api/runners/%s/codec", projectID)
 
-	payload := strings.NewReader("{\n  \"id\": \"0000000000000073\",\n  \"code\": \"\\nfrom codec import Codec\\nimport cbor\\n\\n\\nclass ISRC(Codec):\\n    thing_location = 'loc'\\n\\n    def decode(self, data):\\n        print(\\\"Hello\\\")\\n        d = cbor.loads(data)\\n\\n        if 'lat' in d and 'lng' in d:\\n            d['loc'] = self.create_location(d['lat'], d['lng'])\\n            del d['lat']\\n            del d['lng']\\n\\n        return d\\n\\n    def encode(self, data):\\n        return cbor.dumps(data)\\n\"\n}")
+	code, _ := ioutil.ReadFile("codec.py")
 
-	req, _ := http.NewRequest("POST", url, payload)
+	payload, _ := json.Marshal(struct {
+		ID   string `json:"id"`
+		Code string `json:"code"`
+	}{
+		ID:   "0000000000000073",
+		Code: string(code),
+	})
+
+	req, _ := http.NewRequest("POST", url, bytes.NewReader(payload))
 
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Cache-Control", "no-cache")
-	req.Header.Add("Postman-Token", "0b98cd48-9533-49e4-a18e-45236983f366")
 
-	res, _ := http.DefaultClient.Do(req)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println(res)
 	fmt.Println(string(body))
