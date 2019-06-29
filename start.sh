6d2d9b90e5525fd633bd7d79859e56755d38f3be
Parham Alvani <parham.alvani@gmail.com>
2019-06-29 20:35:25 +0000

    feat: Add Dependencies and Monitoring Services

--- start.sh
+++ start.sh
@@ -7,18 +7,37 @@
 #
 # [] Created By : Parham Alvani <parham.alvani@gmail.com>
 # =======================================
-start-loraserver() {
-        docker-compose -f loraserver.io/docker-compose.yml $@
+
+# dependencies
+
+up-network() {
+        docker network create i1820
 }
 
-start-lanserver() {
-	local name=""
-
-        docker-compose -f lanserver/docker-compose.yml $@
+up-loraserver() {
+        docker-compose -f loraserver.io/docker-compose.yml up -d
 }
 
-start-mongodb() {
-        docker-compose -f mongodb/docker-compose.yml $@
+up-mongodb() {
+        docker-compose -f mongodb/docker-compose.yml up -d
+}
+
+up-vernemq() {
+        docker-compose -f vernemq/docker-compose.yml up -d
+}
+
+# monitoring
+
+up-prometheus() {
+        docker-compose -f prometheus/docker-compose.yml $@
+}
+
+up-portainer() {
+        docker-compose -f portainer/docker-compose.yml $@
+}
+
+up-grafana() {
+        docker-compose -f grafana/docker-compose.yml $@
 }
 
 start-pm() {
@@ -42,23 +61,11 @@ start-downlink() {
         docker-compose -f downlink/docker-compose.yml $@
 }
 
-start-redis() {
-        docker-compose -f redis/docker-compose.yml $@
-}
-
-start-prometheus() {
-        docker-compose -f prometheus/docker-compose.yml $@
-}
-
-start-portainer() {
-        local name=""
-
-        docker-compose -f portainer/docker-compose.yml $@
+start-lanserver() {
+        docker-compose -f lanserver/docker-compose.yml $@
 }
 
 start-gm() {
-        local name=""
-
         docker-compose -f gm/docker-compose.yml $@
 }
 
@@ -77,44 +84,61 @@ start-cleanup() {
         done
 }
 
-usage() {
-        echo "usage: start.sh <service> [args]"
-        echo "services:"
+declare -A groups
+groups=(
+        ["dependencies"]="network mongodb vernemq loraserver"
+        ["monitoring"]="portainer prometheus grafana"
+)
 
-        echo "mongodb           docker-compose mongodb"
-        echo "loraserver        docker-compose brocaar/loraserver"
-        echo "prometheus        docker-compose prom/prometheus"
-        echo "portainer         docker-compose portainer/portainer"
+usage() {
+        echo "usage: start.sh <group> <up/teardown>"
         echo
-        echo "dm         docker-compose aiotrc/dm"
-        echo "pm         docker-compose aiotrc/pm"
-        echo "gm         docker-compose aiotrc/gm"
-        echo "uplink     docker-compose aiotrc/uplink"
-        echo "downlink   docker-compose aiotrc/downlink"
-        echo "lanserver  docker-compose aiotrc/lanserver"
+        echo "each group contains the specific service of I1820 platform"
+        echo "this script will help you to run the signle instance versio of I1820 platfrom"
+        echo "by configuring the script in your way you can run I1820 in different environments"
         echo
+
+        echo "groups:"
+
+        echo "1) dependencies"
+        echo "network:    I1820 private network"
+        echo "mongodb:    The most popular database for modern apps"
+        echo "vernemq:    A distributed MQTT message broker based on Erlang/OTP"
+        echo "loraserver: LoRa Server is an open-source LoRaWAN network-server"
+        echo
+
+        echo "2) monitoring"
+        echo
+
+
         echo "uprojects platfrom users project/redis dockers"
         echo "cleanup   cleans the database up"
 }
 
-if [[ $# -eq 0 ]]; then
+if [[ $# -ne 2 ]]; then
         usage
         exit
 fi
 
-cmd=$1
-shift
-if [ $(type -t start-$cmd)"" = 'function' ]; then
-        echo "Start $cmd"
-        echo
+group=$1
+cmd=$2
+
+if [ $cmd != "up" ] && [ $cmd != "teardown" ]; then
+        usage
+        exit
+fi
+
+services=${groups[$group]}
+
+for service in $services; do
+        echo "> $service"
+
         start=$(date +'%s')
 
-        "start-$cmd" "$@"
+        "$cmd-$service"
 
-        echo
         took=$(( $(date +'%s') - $start ))
         printf "Done. Took %ds.\n" $took
-else
-        echo "Unknown service: $cmd"
-        usage
-fi
+
+        echo ">"
+done
